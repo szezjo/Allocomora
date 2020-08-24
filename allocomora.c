f883460abfee2d72c41444dbce1f76b7762d6576
szezjo <szezjo@gmail.com>
2020-08-24 18:05:40 +0200

    Descriptions added

--- allocomora.c
+++ allocomora.c
@@ -6,10 +6,13 @@
 #include "custom_unistd.h"
 
 // Control number: 110
+
+// Static variables
 static struct heap_t heap;
 static pthread_mutex_t heap_mtx;
 static pthread_mutexattr_t heap_mtxa;
 
+// Heap basic functions
 int heap_setup() {
     if(heap.is_set) {
         printf("Heap is already set up.\n");
@@ -78,129 +81,7 @@ int heap_reset(int force_mode) {
     return heap_setup();
 }
 
-void heap_free(void* memblock) {
-    pthread_mutex_lock(&heap_mtx);
-    if(get_pointer_type(memblock)!=pointer_valid) {
-        pthread_mutex_unlock(&heap_mtx);
-        return;
-    }
-    struct chunk_t *chunk = (struct chunk_t *)((char*)memblock-sizeof(struct chunk_t));
-    chunk->alloc=0;
-
-    if(chunk->prev!=NULL && chunk->prev->alloc==0) chunk=merge(chunk->prev,chunk,1);
-    if(chunk->next!=NULL && chunk->next->alloc==0) chunk=merge(chunk,chunk->next,1);
-
-    update_chunk_checksum(chunk);
-    update_heap_data();
-    pthread_mutex_unlock(&heap_mtx);
-}
-
-void update_end_fence() {
-    pthread_mutex_lock(&heap_mtx);
-    int end_fence=LASFENCE;
-    int *end_fence_e=(int*)(heap.tail_chunk+sizeof(struct chunk_t)+heap.tail_chunk->size);
-    memcpy(end_fence_e,&end_fence,sizeof(int));
-    heap.end_fence_p=end_fence_e;
-    update_heap_checksum();
-    pthread_mutex_unlock(&heap_mtx);
-}
-
-void update_chunk_checksum(struct chunk_t *chunk) {
-    chunk->checksum=1;
-    int newsum=0;
-    for(int i=0; i<sizeof(struct chunk_t); i++) {
-        newsum+=*(((char*)chunk)+i);
-    }
-    chunk->checksum=newsum;
-}
-
-void update_heap_checksum() {
-    heap.checksum=1;
-    int newsum=0;
-    struct heap_t *p = &heap;
-    for(int i=0; i<sizeof(struct heap_t); i++) {
-        newsum+=*(((char*)p)+i);
-    }
-    heap.checksum=newsum;
-}
-
-int verify_chunk_checksum(struct chunk_t *chunk) {
-    int oldsum=chunk->checksum;
-    update_chunk_checksum(chunk);
-    int newsum=chunk->checksum;
-    chunk->checksum=oldsum;
-    if(oldsum!=newsum) return 1;
-    return 0;
-}
-
-int verify_heap_checksum() {
-    int oldsum=heap.checksum;
-    update_heap_checksum();
-    int newsum=heap.checksum;
-    heap.checksum=oldsum;
-    if(oldsum!=newsum) return 1;
-    return 0;
-}
-
-void *find_free_chunk(size_t size) {
-    struct chunk_t *chunk_to_check=heap.head_chunk;
-    size_t best_fit_size=-1;
-    struct chunk_t *best_fit=NULL;
-    while(chunk_to_check!=NULL) {
-        if(chunk_to_check->alloc==0) {
-            if((chunk_to_check->size)>(size+sizeof(struct chunk_t))) {
-                if(best_fit_size==-1 || best_fit_size>(chunk_to_check->size)) {
-                    best_fit_size=chunk_to_check->size;
-                    best_fit=chunk_to_check;
-                }
-            }
-            else if(chunk_to_check->size==size) {
-                printf("Found chunk with size %lu\n", size);
-                return chunk_to_check;
-            }
-        }
-        chunk_to_check=chunk_to_check->next;
-    }
-    return best_fit;
-}
-
-enum pointer_type_t get_pointer_type(const void* pointer) {
-    if(pointer==NULL) return pointer_null;
-    const char *p = (char*)pointer; // to perform pointer arithmetic
-    if(p<(char*)heap.data || p>=(char*)(heap.tail_chunk+heap.tail_chunk->size+sizeof(struct chunk_t)+sizeof(int))) return pointer_out_of_heap;
-    if(p>=(char*)heap.end_fence_p && p<(char*)heap.end_fence_p+sizeof(int)) return pointer_end_fence;
-    struct chunk_t *i=heap.head_chunk;
-    while(i!=NULL) {
-        if(p>=(char*)i && p<(char*)i+sizeof(struct chunk_t)+i->size) {
-            if(p>=(char*)i && p<(char*)i+sizeof(struct chunk_t)) return pointer_control_block;
-            if(i->alloc==0) return pointer_unallocated;
-            if(p==(char*)i+sizeof(struct chunk_t)) return pointer_valid;
-            return pointer_inside_data_block;
-        }
-        i=i->next;
-    }
-    return pointer_out_of_heap;
-}
-
-void print_pointer_type(const void* pointer) {
-    enum pointer_type_t type = get_pointer_type(pointer);
-    if(type==pointer_null) printf("[%p] is null\n", pointer);
-    else if (type==pointer_out_of_heap) printf("[%p] is out of heap\n", pointer);
-    else if (type==pointer_control_block) printf("[%p] is inside control block\n", pointer);
-    else if (type==pointer_inside_data_block) printf("[%p] is inside data block\n", pointer);
-    else if (type==pointer_unallocated) printf("[%p] is unallocated\n", pointer);
-    else if (type==pointer_valid) printf("[%p] is valid\n",pointer);
-    else if (type==pointer_end_fence) printf("[%p] is at the end fence\n", pointer);
-}
-
-void update_heap_data() {
-    struct chunk_t *ch = heap.head_chunk;
-    while(ch->next!=NULL) ch=ch->next;
-    heap.tail_chunk=ch;
-
-    update_heap_checksum();
-}
-
+// *alloc functions
 void *heap_malloc_debug(size_t count, int fileline, const char* filename) {
     pthread_mutex_lock(&heap_mtx);
     struct chunk_t *chunk_to_alloc = find_free_chunk(count);
@@ -308,16 +189,7 @@ void *heap_realloc_debug(void *memblock, size_t size, int fileline, const char *
     return p;
 }
 
-void *heap_malloc(size_t count) {
-    return heap_malloc_debug(count,0,NULL);
-}
-void *heap_calloc(size_t number, size_t size) {
-    return heap_calloc_debug(number,size,0,NULL);
-}
-void *heap_realloc(void *memblock, size_t size) {
-    return heap_realloc_debug(memblock,size,0,NULL);
-}
-
+// *alloc_aligned functions
 void *heap_malloc_aligned_debug(size_t count, int fileline, const char *filename) {
     if(heap.pages<2) return NULL;
     pthread_mutex_lock(&heap_mtx);
@@ -447,6 +319,16 @@ void *heap_realloc_aligned_debug(void *memblock, size_t size, int fileline, cons
     return p;
 }
 
+// Non-debug *alloc and *alloc_aligned functions
+void *heap_malloc(size_t count) {
+    return heap_malloc_debug(count,0,NULL);
+}
+void *heap_calloc(size_t number, size_t size) {
+    return heap_calloc_debug(number,size,0,NULL);
+}
+void *heap_realloc(void *memblock, size_t size) {
+    return heap_realloc_debug(memblock,size,0,NULL);
+}
 void *heap_malloc_aligned(size_t count) {
     return heap_malloc_aligned_debug(count,0,NULL);
 }
@@ -456,6 +338,25 @@ void *heap_calloc_aligned(size_t number, size_t size) {
 void *heap_realloc_aligned(void* memblock, size_t size) {
     return heap_realloc_aligned_debug(memblock,size,0,NULL);
 }
+
+// Chunk management functions
+void heap_free(void* memblock) {
+    pthread_mutex_lock(&heap_mtx);
+    if(get_pointer_type(memblock)!=pointer_valid) {
+        pthread_mutex_unlock(&heap_mtx);
+        return;
+    }
+    struct chunk_t *chunk = (struct chunk_t *)((char*)memblock-sizeof(struct chunk_t));
+    chunk->alloc=0;
+
+    if(chunk->prev!=NULL && chunk->prev->alloc==0) chunk=merge(chunk->prev,chunk,1);
+    if(chunk->next!=NULL && chunk->next->alloc==0) chunk=merge(chunk,chunk->next,1);
+
+    update_chunk_checksum(chunk);
+    update_heap_data();
+    pthread_mutex_unlock(&heap_mtx);
+}
+
 struct chunk_t *merge(struct chunk_t *chunk1, struct chunk_t *chunk2, char safe_mode) {
     if(chunk1==NULL || chunk2==NULL) return NULL;
     if(chunk2->next==chunk1) return merge(chunk2, chunk1, safe_mode);
@@ -506,6 +407,66 @@ struct chunk_t *split(struct chunk_t *chunk_to_split, size_t size) {
     return chunk_to_split;
 }
 
+void *find_free_chunk(size_t size) {
+    struct chunk_t *chunk_to_check=heap.head_chunk;
+    size_t best_fit_size=-1;
+    struct chunk_t *best_fit=NULL;
+    while(chunk_to_check!=NULL) {
+        if(chunk_to_check->alloc==0) {
+            if((chunk_to_check->size)>(size+sizeof(struct chunk_t))) {
+                if(best_fit_size==-1 || best_fit_size>(chunk_to_check->size)) {
+                    best_fit_size=chunk_to_check->size;
+                    best_fit=chunk_to_check;
+                }
+            }
+            else if(chunk_to_check->size==size) {
+                printf("Found chunk with size %lu\n", size);
+                return chunk_to_check;
+            }
+        }
+        chunk_to_check=chunk_to_check->next;
+    }
+    return best_fit;
+}
+
+// Heap control functions
+enum pointer_type_t get_pointer_type(const void* pointer) {
+    if(pointer==NULL) return pointer_null;
+    const char *p = (char*)pointer; // to perform pointer arithmetic
+    if(p<(char*)heap.data || p>=(char*)(heap.tail_chunk+heap.tail_chunk->size+sizeof(struct chunk_t)+sizeof(int))) return pointer_out_of_heap;
+    if(p>=(char*)heap.end_fence_p && p<(char*)heap.end_fence_p+sizeof(int)) return pointer_end_fence;
+    struct chunk_t *i=heap.head_chunk;
+    while(i!=NULL) {
+        if(p>=(char*)i && p<(char*)i+sizeof(struct chunk_t)+i->size) {
+            if(p>=(char*)i && p<(char*)i+sizeof(struct chunk_t)) return pointer_control_block;
+            if(i->alloc==0) return pointer_unallocated;
+            if(p==(char*)i+sizeof(struct chunk_t)) return pointer_valid;
+            return pointer_inside_data_block;
+        }
+        i=i->next;
+    }
+    return pointer_out_of_heap;
+}
+
+void update_heap_data() {
+    struct chunk_t *ch = heap.head_chunk;
+    while(ch->next!=NULL) ch=ch->next;
+    heap.tail_chunk=ch;
+
+    update_heap_checksum();
+}
+
+void update_end_fence() {
+    pthread_mutex_lock(&heap_mtx);
+    int end_fence=LASFENCE;
+    int *end_fence_e=(int*)(heap.tail_chunk+sizeof(struct chunk_t)+heap.tail_chunk->size);
+    memcpy(end_fence_e,&end_fence,sizeof(int));
+    heap.end_fence_p=end_fence_e;
+    update_heap_checksum();
+    pthread_mutex_unlock(&heap_mtx);
+}
+
+// Statistics functions
 void* heap_get_data_block_start(const void* pointer) {
     enum pointer_type_t type = get_pointer_type(pointer);
     if(type==pointer_valid) return (void*)pointer;
@@ -586,6 +547,45 @@ size_t heap_get_block_size(const void* memblock) {
     return tmp->size;
 }
 
+// Checksum functions
+void update_chunk_checksum(struct chunk_t *chunk) {
+    chunk->checksum=1;
+    int newsum=0;
+    for(int i=0; i<sizeof(struct chunk_t); i++) {
+        newsum+=*(((char*)chunk)+i);
+    }
+    chunk->checksum=newsum;
+}
+
+void update_heap_checksum() {
+    heap.checksum=1;
+    int newsum=0;
+    struct heap_t *p = &heap;
+    for(int i=0; i<sizeof(struct heap_t); i++) {
+        newsum+=*(((char*)p)+i);
+    }
+    heap.checksum=newsum;
+}
+
+int verify_chunk_checksum(struct chunk_t *chunk) {
+    int oldsum=chunk->checksum;
+    update_chunk_checksum(chunk);
+    int newsum=chunk->checksum;
+    chunk->checksum=oldsum;
+    if(oldsum!=newsum) return 1;
+    return 0;
+}
+
+int verify_heap_checksum() {
+    int oldsum=heap.checksum;
+    update_heap_checksum();
+    int newsum=heap.checksum;
+    heap.checksum=oldsum;
+    if(oldsum!=newsum) return 1;
+    return 0;
+}
+
+// Calculation functions
 size_t calc_dist(struct chunk_t *chunk) {
     struct chunk_t *p = heap.head_chunk;
     size_t dist=0;
@@ -610,6 +610,8 @@ int is_aligned(size_t dist) {
     return !!(dist%PAGE_SIZE);
 }
 
+
+// Validation functions
 enum validation_code_t heap_validate() {
     if(verify_heap_checksum()) return err_heap_checksum;
     if(heap.head_chunk==NULL) return err_head_is_null;
@@ -649,6 +651,7 @@ enum validation_code_t validate_and_print() {
     return ret;
 }
 
+// Debug dump functions
 void heap_dump_debug_information() {
     printf("\n***  HEAP INFO  ***\n");
     struct chunk_t *p = heap.head_chunk;
@@ -673,6 +676,18 @@ void heap_dump_debug_information() {
 
     printf("\n*******************\n"); 
 }
+
+void print_pointer_type(const void* pointer) {
+    enum pointer_type_t type = get_pointer_type(pointer);
+    if(type==pointer_null) printf("[%p] is null\n", pointer);
+    else if (type==pointer_out_of_heap) printf("[%p] is out of heap\n", pointer);
+    else if (type==pointer_control_block) printf("[%p] is inside control block\n", pointer);
+    else if (type==pointer_inside_data_block) printf("[%p] is inside data block\n", pointer);
+    else if (type==pointer_unallocated) printf("[%p] is unallocated\n", pointer);
+    else if (type==pointer_valid) printf("[%p] is valid\n",pointer);
+    else if (type==pointer_end_fence) printf("[%p] is at the end fence\n", pointer);
+}
+
 
 int main() {
     int tmp = heap_setup();
--- allocomora.h
+++ allocomora.h
@@ -1,6 +1,7 @@
 #ifndef ALLOCOMORA_H
 #define ALLOCOMORA_H
 
+// Basic data
 #define KB 1024
 #define PAGE_SIZE (4*KB)
 #define PAGES_BGN 2
@@ -8,6 +9,7 @@
 #define SECFENCE 495105411
 #define LASFENCE 693452304
 
+// Structures
 struct chunk_t {
     int first_fence;
     struct chunk_t *prev;
@@ -26,12 +28,12 @@ struct heap_t {
     int *end_fence_p;
     char is_set;
     void *data;
-    //size_t size;
     int pages;
     int chunks;
     int checksum;
 };
 
+// Enums
 enum pointer_type_t
 {
       pointer_null,
@@ -58,26 +60,41 @@ enum validation_code_t {
     err_invalid_tail
 };
 
+// Heap basic functions
 int heap_setup();
 int heap_delete(int safe_mode);
 int heap_reset();
+
+// *alloc functions
 void *heap_malloc_debug(size_t count, int fileline, const char* filename);
 void* heap_calloc_debug(size_t number, size_t size, int fileline, const char* filename); 
 void* heap_realloc_debug(void* memblock, size_t size, int fileline, const char* filename);
-void *heap_malloc(size_t count);
-void *heap_calloc(size_t number, size_t size);
-void *heap_realloc(void* memblock, size_t size);
+
+// *alloc_aligned functions
 void *heap_malloc_aligned_debug(size_t count, int fileline, const char *filename);
 void* heap_calloc_aligned_debug(size_t number, size_t size, int fileline, const char* filename); 
 void* heap_realloc_aligned_debug(void* memblock, size_t size, int fileline, const char* filename);
+
+// Non-debug *alloc and *alloc_aligned functions
+void *heap_malloc(size_t count);
+void *heap_calloc(size_t number, size_t size);
+void *heap_realloc(void* memblock, size_t size);
 void *heap_malloc_aligned(size_t count);
 void *heap_calloc_aligned(size_t number, size_t size);
 void *heap_realloc_aligned(void* memblock, size_t size);
+
+// Chunk management functions
 void heap_free(void* memblock);
+struct chunk_t *merge(struct chunk_t *chunk1, struct chunk_t *chunk2, char safe_mode);
+struct chunk_t *split(struct chunk_t *chunk_to_split, size_t size);
 void *find_free_chunk(size_t size);
+
+// Heap control functions
 enum pointer_type_t get_pointer_type(const void* pointer);
 void update_heap_data();
 void update_end_fence();
+
+// Statistics functions
 void* heap_get_data_block_start(const void* pointer);
 size_t heap_get_used_space(void);
 size_t heap_get_largest_used_block_size(void);
@@ -86,17 +103,24 @@ size_t heap_get_free_space(void);
 size_t heap_get_largest_free_area(void);
 uint64_t heap_get_free_gaps_count(void);
 size_t heap_get_block_size(const void* memblock);
+
+// Checksum functions
 void update_chunk_checksum(struct chunk_t *chunk);
 void update_heap_checksum();
 int verify_chunk_checksum(struct chunk_t *chunk);
 int verify_heap_checksum();
+
+// Calculation functions
 size_t calc_dist(struct chunk_t *chunk);
 size_t calc_size_in_page(struct chunk_t *chunk, size_t dist);
 int is_aligned(size_t dist);
-enum validation_code_t heap_validate(void);
-void heap_dump_debug_information(void);
-struct chunk_t *merge(struct chunk_t *chunk1, struct chunk_t *chunk2, char safe_mode);
-struct chunk_t *split(struct chunk_t *chunk_to_split, size_t size);
 
-//change test vscode 
+// Validation functions
+enum validation_code_t heap_validate(void);
+enum validation_code_t validate_and_print();
+
+// Debug dump functions
+void heap_dump_debug_information(void);
+void print_pointer_type(const void* pointer);
+
 #endif //ALLOCOMORA_H
